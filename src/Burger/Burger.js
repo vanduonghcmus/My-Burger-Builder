d2dba7f91a8f1c64e46bd779989e39d848597932
DuongAmin <vanduonghcmus@example.com>
2020-07-31 22:25:28 +0700

    update state and update BurgerIngredient

---
+++ src/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import styles from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+		debugger;
+		console.log(props.ingredients[igKey]);
+		return [...Array(props.ingredients[igKey])].map((_, i) => {
+			return <BurgerIngredient key={igKey + i} type={igKey} />;
+		});
+	});
+
+	return (
+		<div className={styles.Burger}>
+			<BurgerIngredient type="bread-top" />
+			{transformedIngredients}
+			<BurgerIngredient type="bread-bottom" />
+		</div>
+	);
+};
+
+export default burger;
--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+
 import Aux from '../hoc/Auxiliary';
+import BurgerIngredient from '../Burger/Burger';
 
 class BurgerBuilder extends Component {
+	// constructor(props){
+	// 	super(props),
+
+	// }
+
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 1,
+			cheese: 2,
+			meat: 2,
+		},
+	};
+
 	render() {
 		return (
 			<Aux>
-				<div>Burger</div>
+				<BurgerIngredient ingredients={this.state.ingredients} />
 				<div>Burger Controls</div>
 			</Aux>
 		);
