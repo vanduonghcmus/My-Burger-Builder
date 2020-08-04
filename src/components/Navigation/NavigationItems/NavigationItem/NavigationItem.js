0b77f944ad88845941ffc01fd27f2aef2fac1415
DuongAmin <vanduonghcmus@example.com>
2020-08-04 17:03:57 +0700

    add Navigation Items

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './NavigationItem.module.css';
+
+const navigationItem = (props) => (
+	<li className={styles.NavigationItem}>
+		<a href={props.link} className={props.active === true && styles.active}>
+			{props.children}
+		</a>
+	</li>
+);
+
+export default navigationItem;
