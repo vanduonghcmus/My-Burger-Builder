7cfcfed3e5a971362c4787c91551a7cf2d7ddfb1
Duong Nguyen <you@example.com>
2020-09-13 11:49:24 +0700

    fetch ingredients asynchorouns, initializing it in burgerBuilder

--- src/store/action/burgerBuilder.js
+++ src/store/action/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "../../axios-burgerSummary";
 
 export const addIngredient = (name) => {
   return {
@@ -11,5 +12,32 @@ export const removeIngredient = (name) => {
   return {
     type: actionTypes.REMOVE_INGREDIENT,
     ingredientName: name,
+  };
+};
+
+export const setIngredients = (ingredients) => {
+  return {
+    type: actionTypes.SET_INGREDIENTS,
+    ingredients: ingredients,
+  };
+};
+
+export const fetchIngredientFailed = () => {
+  return {
+    type: actionTypes.FETCH_INGREDIENTS_FAILED,
+  };
+};
+
+// asynchronous action
+export const initIngredients = () => {
+  return (dispatch) => {
+    axios
+      .get("https://react-my-burger-57df8.firebaseio.com/ingredients.json")
+      .then((res) => {
+        dispatch(setIngredients(res.data));
+      })
+      .catch((err) => {
+        dispatch(fetchIngredientFailed());
+      });
   };
 };
