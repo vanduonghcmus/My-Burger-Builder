38e2b19234a77646d8068cc22c04ce565978b445
Duong Nguyen <you@example.com>
2020-09-17 13:34:01 +0700

    guarding Router

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,39 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Order.module.css';
+import styles from "./Order.module.css";
 
 const Order = (props) => {
-	const ingrdients = [];
+  const ingredient = [];
 
-	for (let ingredientName in props.ingredients) {
-		ingrdients.push({
-			name: ingredientName,
-			amount: props.ingredients[ingredientName],
-		});
-		console.log(props.ingredients[ingredientName]);
-	}
+  for (let ingredientName in props.ingredients) {
+    ingredient.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName],
+    });
+  }
 
-	const ingredientOutput = ingrdients.map((ig) => (
-		<span
-			style={{
-				textTransform: 'capitalize',
-				display: 'inline-block',
-				padding: '5px',
-				border: '1px solid #ccc',
-				margin: '0px 8px',
-			}}>
-			{ig.name} ({ig.amount})
-		</span>
-	));
+  const ingredientOutput = ingredient.map((ig) => (
+    <span
+      style={{
+        textTransform: "capitalize",
+        display: "inline-block",
+        padding: "5px",
+        border: "1px solid #ccc",
+        margin: "0px 8px",
+      }}
+    >
+      {ig.name} ({ig.amount})
+    </span>
+  ));
 
-	return (
-		<div className={styles.Order}>
-			<p>Ingredients: {ingredientOutput} </p>
-			<p>
-				Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
-			</p>
-		</div>
-	);
+  return (
+    <div className={styles.Order}>
+      <p>Ingredients: {ingredientOutput} </p>
+      <p>
+        Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+      </p>
+    </div>
+  );
 };
 
 export default Order;
