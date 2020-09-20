07ecb1e25350df0eb38f5b629654e7e97337f415
Duong Nguyen <you@example.com>
2020-09-20 10:05:26 +0700

    handling authencation

--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -1,4 +1,5 @@
 import { put, delay } from "redux-saga/effects";
+import axios from "axios";
 
 import * as actions from "../store/action/index";
 
@@ -10,6 +11,37 @@ export function* logoutSaga(action) {
 }
 
 export function* checkAuthTimeoutSaga(action) {
-  yield delay(action.expirationTime);
+  yield delay(action.expirationTime * 1000); // in millisecond;
   yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+  yield put(actions.authStart());
+  const authData = {
+    email: action.email,
+    password: action.password,
+    returnSecureToken: true,
+  };
+  let url =
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAYRpRXcntjq8RFh0TEyKCDJTBOLWr_KS8";
+  if (!action.isSignup) {
+    url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAYRpRXcntjq8RFh0TEyKCDJTBOLWr_KS8";
+  }
+  try {
+    const response = yield axios.post(url, authData);
+
+    const expirationDate = yield new Date(
+      new Date().getTime() + response.data.expiresIn * 1000
+    );
+    yield localStorage.setItem("expirationDate", expirationDate);
+    yield localStorage.setItem("token", response.data.idToken);
+    yield localStorage.setItem("userId", response.data.localId);
+    yield put(
+      actions.authSuccess(response.data.idToken, response.data.localId)
+    );
+    yield put(actions.checkAuthTimeout(response.data.expiresIn)); // in second
+  } catch (error) {
+    yield put(actions.authFail(error.response.data.error));
+  }
 }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,10 @@
 import { takeEvery } from "redux-saga/effects";
 
-import { logoutSaga, checkAuthTimeoutSaga } from "./auth";
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from "./auth";
 import * as actionTypes from "../store/action/actionTypes";
 
 export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+  yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
 }
