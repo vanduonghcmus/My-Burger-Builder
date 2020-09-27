1a01ca8cca9df174c9bb40d699dfa96f3417abe1
vanduonghcmus <58739835+vanduonghcmus@users.noreply.github.com>
2020-09-27 16:27:59 +0700

    add custom error handling

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Aux from "../Auxiliary/Auxiliary";
 import Modal from "../../UI/Modal/Modal";
+import useHttpErrorHandler from "../../hooks/http-errorHandler";
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return (props) => {
-    const [error, setError] = useState(null);
-
-    // when requested, the error must be null
-    const reqInterceptor = axios.interceptors.request.use((req) => {
-      setError(null);
-      return req;
-    });
-
-    const resInterceptor = axios.interceptors.response.use(
-      (res) => res,
-      (err) => {
-        setError(err);
-      }
-    );
-
-    useEffect(() => {
-      axios.interceptors.request.eject(reqInterceptor);
-      axios.interceptors.response.eject(resInterceptor);
-    }, [reqInterceptor, resInterceptor]);
-
-    const errorConfirmedHandler = () => {
-      setError(null);
-    };
+    const [error, errorClear] = useHttpErrorHandler(axios);
 
     return (
       <Aux>
-        <Modal show={error} modalClosed={errorConfirmedHandler}>
+        <Modal show={error} modalClosed={errorClear}>
           {error ? error.message : null}
         </Modal>
         <WrappedComponent {...props} />
---
+++ src/hooks/http-errorHandler.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default (httpClient) => {
+  const [error, setError] = useState(null);
+
+  // when requested, the error must be null
+  const reqInterceptor = httpClient.interceptors.request.use((req) => {
+    setError(null);
+    return req;
+  });
+
+  const resInterceptor = httpClient.interceptors.response.use(
+    (res) => res,
+    (err) => {
+      setError(err);
+    }
+  );
+
+  useEffect(() => {
+    httpClient.interceptors.request.eject(reqInterceptor);
+    httpClient.interceptors.response.eject(resInterceptor);
+  }, [reqInterceptor, resInterceptor]);
+
+  const errorConfirmedHandler = () => {
+    setError(null);
+  };
+
+  return [error, errorConfirmedHandler];
+};
