b1d833ff0d14194e7d21a88a40495dcf162dc05d
vanduonghcmus <58739835+vanduonghcmus@users.noreply.github.com>
2020-09-27 15:02:30 +0700

    update component and Logout
    add hook to component
    use React.memo in Modal

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,39 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import styles from "./Modal.module.css";
 import Aux from "../../hoc/Auxiliary/Auxiliary";
 import Backdrop from "../BackDrop/Backdrop";
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProp, nextState) {
-    return (
-      nextProp.show !== this.props.show ||
-      nextProp.children !== this.props.children
-    );
-  }
+const Modal = (props) => {
+  // shouldComponentUpdate(nextProp, nextState) {
+  //   return (
+  //
+  //   );
+  // }
 
-  render() {
-    return (
-      <Aux>
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-        <div
-          className={styles.Modal}
-          style={{
-            transform: this.props.show ? "translateY(0)" : "translate(-100vh)",
-            opacity: this.props.show ? "1" : "0",
-          }}
-        >
-          {this.props.children}
-        </div>
-      </Aux>
-    );
-  }
-}
+  return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={styles.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translate(-100vh)",
+          opacity: props.show ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
 
 Modal.propTypes = {
   show: PropTypes.bool,
   modalClosed: PropTypes.func,
 };
 
-export default Modal;
+export default React.memo(
+  Modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+);
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,43 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import Aux from "../../../hoc/Auxiliary/Auxiliary";
 import Button from "../../../UI/Button/Button";
 
-class OrderSummary extends Component {
+const OrderSummary = (props) => {
   // this could be a functional component, doesn't have to be a class-component
-
-  render() {
-    // this.props.ingredients = null;
-    // debugger;
-    const ingredientsSummary = Object.keys(this.props.ingredients).map(
-      (igKey) => {
-        return (
-          <li key={igKey}>
-            <span>{igKey}</span>: {this.props.ingredients[igKey]}
-          </li>
-        );
-      }
-    );
+  // this.props.ingredients = null;
+  // debugger;
+  const ingredientsSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-      <Aux>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the folowing ingredients: </p>
-        <ul>{ingredientsSummary}</ul>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}$</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button clicked={this.props.purchaseCancelled} btnType="Danger">
-          CANCEL
-        </Button>
-        <Button clicked={this.props.purchaseContinued} btnType="Success">
-          CONTINUE
-        </Button>
-      </Aux>
+      <li key={igKey}>
+        <span>{igKey}</span>: {props.ingredients[igKey]}
+      </li>
     );
-  }
-}
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients: </p>
+      <ul>{ingredientsSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}$</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button clicked={props.purchaseCancelled} btnType="Danger">
+        CANCEL
+      </Button>
+      <Button clicked={props.purchaseContinued} btnType="Success">
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
 
 OrderSummary.propTypes = {
   purchaseContinued: PropTypes.func,
