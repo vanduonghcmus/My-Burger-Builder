1767002076a60a41fa92e25adf12e06eb3d0d75d
vanduonghcmus <58739835+vanduonghcmus@users.noreply.github.com>
2020-09-27 14:44:05 +0700

    update Auth and Logout
    add react-hook to Auth and Logout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 import Aux from "../../hoc/Auxiliary/Auxiliary";
